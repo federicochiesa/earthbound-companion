9f6f4e672f98e204b2b2fbf1b9f9a1ff965b205d
Federico Chiesa <federico.chiesa@me.com>
2022-03-21 19:04:14 +0100

    add map list and initial maps

---
+++ scripts/maps.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    document.getElementById("mapiFrame").src = "../../mapframe/dist/?region=onett"
+    var list = document.querySelectorAll(".list-group-item");
+    for (var i = 0; i < list.length; ++i) list[i].classList.remove('active');
+    document.getElementById("onett").classList.add("active");
+}
+
+function updateMap(id){
+    console.log(id);
+    var list = document.querySelectorAll(".list-group-item");
+    for (var i = 0; i < list.length; ++i) list[i].classList.remove('active');
+    document.getElementById(id).classList.add("active");
+    document.getElementById("mapiFrame").src = "../../mapframe/dist/?region="+id;
+}
