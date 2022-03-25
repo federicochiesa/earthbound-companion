92d012b1c59c2f8e5abb498877aeef61e61d017d
andrealeonardo <andreardo.9898@gamil.com>
2022-03-25 19:43:44 +0100

    add level selector

---
+++ scripts/equipmenttesting.js
@@ -0,0 +1,11 @@
+async function getData(type){
+    let data = await fetch('../../data/' + type + '.json');
+    data = data.json();
+    return data;
+}
+
+window.onload = async function(){
+    let data = await getData("stats");
+    let list = document.getElementById("dataList");
+
+}
