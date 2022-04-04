4ee9beab5401e213d74838bd5ac02ac9cc6aa8cf
Federico Chiesa <federico.chiesa@me.com>
2022-04-04 11:32:55 +0200

    add more info data for enemies page

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -1,6 +1,6 @@
-function showInfoModal(item) {
+async function showInfoModal(item) {
     document.getElementById("infoModalTitle").textContent = "More info: " + item;
-    document.getElementById("infoModalBody").textContent = "This is a test info modal for " + item;
+    document.getElementById("infoModalBody").innerHTML = await getEnemyInfo(item);
     new bootstrap.Modal(document.getElementById('infoModal')).show();
 }
 
@@ -8,6 +8,67 @@ function showMapModal(item) {
     document.getElementById("mapModalTitle").textContent = "Map: " + item;
     document.getElementById("mapModalBody").textContent = "This is a test map modal for " + item;
     new bootstrap.Modal(document.getElementById('mapModal')).show();
+}
+
+async function getEnemyInfo(enemy) {
+    let data = await getData("enemies");
+    for (let i = 0; i < Object.keys(data).length; i++) {
+        let elements = data[Object.keys(data)[i]];
+        for (const element of elements) {
+            if (element.name == enemy) {
+                let returnString = "<table class=\"table table-dark table-striped\">\
+                <tbody>\
+                    <tr>\
+                        <th scope=\"row\">HP</th>\
+                        <td>" + element.data["Hit points"] + "</td>\
+                    </tr>\
+                    <tr>\
+                        <th scope=\"row\">PP</th>\
+                        <td>" + element.data["Psychic points"] + "</td>\
+                    </tr>\
+                    <tr>\
+                        <th scope=\"row\">Attack</th>\
+                        <td>" + element.data["Offense"] + "</td>\
+                    </tr>\
+                    <tr>\
+                        <th scope=\"row\">Defense</th>\
+                        <td>" + element.data["Defense"] + "</td>\
+                    </tr>\
+                    <tr>\
+                        <th scope=\"row\">Speed</th>\
+                        <td>" + element.data["Speed"] + "</td>\
+                    </tr>\
+                    <tr>\
+                        <th scope=\"row\">Location</th>\
+                        <td>" + element.data["Location"] + "</td>\
+                    </tr>";
+                if(typeof(element.data["Guts"]) != "undefined")
+                    returnString += "<tr>\
+                    <th scope=\"row\">Guts</th>\
+                    <td>" + element.data["Guts"] + "</td>\
+                        </tr>"
+                if(typeof(element.data["Item"]) != "undefined")
+                    returnString += "<tr>\
+                    <th scope=\"row\">Item</th>\
+                    <td>" + element.data["Item"] + "</td>\
+                        </tr>"
+                if(typeof(element.data["Exp"]) != "undefined")
+                    returnString += "<tr>\
+                    <th scope=\"row\">Experience</th>\
+                    <td>" + element.data["Exp"] + "</td>\
+                        </tr>"
+                if(typeof(element.data["Money"]) != "undefined")
+                    returnString += "<tr>\
+                    <th scope=\"row\">Money</th>\
+                    <td>" + element.data["Money"] + "</td>\
+                    </tr>"
+
+                returnString += "</tbody>\
+                            </table>";
+                return returnString;
+            }
+        }
+    }
 }
 
 window.onload = async function () {
