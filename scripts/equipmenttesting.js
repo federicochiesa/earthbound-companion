29fc7d39f982abbe34afbd32d67afd6825a15df0
andrealeonardo <andreardo.9898@gamil.com>
2022-04-10 13:46:16 +0200

    fix equipment testing search bug

--- scripts/equipmenttesting.js
+++ scripts/equipmenttesting.js
@@ -61,8 +61,8 @@ window.onload = async function () {
     
     setTimeout(async function(){
         for(let j = 0; j < pages.length; j++){
-            data[j] = await getData(pages[j]);
-            d = data[j]
+            datax[j] = await getData(pages[j]);
+            d = datax[j]
             for (let i = 0; i < Object.keys(d).length; i++) {
                 var elements = d[Object.keys(d)[i]];
                 for(let k = 0; k < elements.length; k++){
