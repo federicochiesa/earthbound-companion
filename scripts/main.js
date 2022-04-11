6ac5e8988747a7c7b7afddc6cd75fc708d19b26b
Federico Chiesa <federico.chiesa@me.com>
2022-04-11 15:20:08 +0200

    limit search results

--- scripts/main.js
+++ scripts/main.js
@@ -126,6 +126,7 @@ function autocomplete(inp, arr) {
       /*append the DIV element as a child of the autocomplete container:*/
       this.parentNode.appendChild(a);
       /*for each item in the array...*/
+      j = 0
       for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
@@ -145,6 +146,9 @@ function autocomplete(inp, arr) {
               closeAllLists();
           });
           a.appendChild(b);
+          if (j > 10) // Limit search suggestions to 10
+            break;
+          j++
         }
       }
   });
